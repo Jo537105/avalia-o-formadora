1078a9502c2f6e771cb5f451de3e949e2427a832
Jo537105 <jardelei.voliveira@gmail.com>
2024-10-12 20:18:46 -0300

    Add files via upload

---
+++ src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { TecladoComponent } from './teclado/teclado.component';
+import { FormsModule } from '@angular/forms';
+import { SobreComponent } from './sobre/sobre.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    TecladoComponent,
+    SobreComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
